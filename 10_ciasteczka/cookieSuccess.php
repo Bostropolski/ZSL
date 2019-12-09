b8447d6329ffd5777dfb3f44e1df6c79190d07e6
Bostropolski <55092030+Bostropolski@users.noreply.github.com>
2019-12-09 13:52:25 +0100

    Update cookieSuccess.php

--- 10_ciasteczka/cookieSuccess.php
+++ 10_ciasteczka/cookieSuccess.php
@@ -3,6 +3,6 @@ session_start();
 session_destroy();
 
 echo "Imie i Nazwisko ",$_SESSION['name'],"<br>";
-echo "Wiek ",$_COOKIE['wiek']"<br>";
-echo "Narodowość ",$_COOKIE['narodowosc']"<br>";
+echo "Wiek ",$_COOKIE['wiek'],"<br>";
+echo "Narodowość ",$_COOKIE['narodowosc'],"<br>";
  ?>
